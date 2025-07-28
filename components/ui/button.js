588392a92be2f523961000780cfb1abd426022bc
lievenvos <lievenvos@gmail.com>
2025-07-28 17:07:08 +0200

    Add files via upload
    eerste try

---
+++ components/ui/button.js
@@ -0,0 +1,7 @@
+export function Button({ children, ...props }) {
+  return (
+    <button {...props} className="bg-blue-600 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded shadow">
+      {children}
+    </button>
+  );
+}
